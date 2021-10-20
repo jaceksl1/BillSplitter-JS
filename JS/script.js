22f999e7573ef83cfe060c387cfbbf41059ebadb
Jacek <87030915+jaceksl1@users.noreply.github.com>
2021-10-20 10:38:22 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,14 @@ const cost = document.querySelector(".cost");
 const costInfo = document.querySelector(".cost-info");
 const error = document.querySelector(".error");
 error.style.display = "none";
+const localData = document.querySelector(".localData");
+
+const now = new Date();
+const elDate = document.querySelector(".element");
+const elDateText = `
+Time: ${now.getHours()} : ${now.getMinutes()} : ${now.getSeconds()}
+Day: ${now.getDate()} . ${now.getMonth()+1} . ${now.getFullYear()}
+`
 
 const checkValue = () => {
   if (price.value === "" || people.value === "" || tip.value === "") {
@@ -24,6 +32,34 @@ const countCost = () => {
   costInfo.style.display = "block";
   cost.textContent = sum.toFixed(2);
   error.style.display = "none";
+  const li = document.createElement("li");
+  li.innerText = sum + "USD" + elDateText;
+  console.log(li);
+  localData.appendChild(li);
+  document.body.append(localData);
+  localStorage.setItem(li, li.innerText);
+  const data = localStorage.getItem(li);
+  console.log(localStorage);
 };
 
-countBtn.addEventListener("click", checkValue);
+countBtn.addEventListener("click", function () {
+  checkValue();
+  const clearBtn = document.querySelector(".clearBtn");
+  clearBtn.style.display = "block";
+  clearBtn.addEventListener("click", function () {
+    costInfo.style.display = "none";
+    people.value = "";
+    price.value = "";
+    tip.value = 0;
+    error.textContent = "";
+    clearBtn.style.display = "none";
+  });
+});
+
+const btnClearLocalStorage = document.querySelector(".btnClearLocalStorage");
+
+btnClearLocalStorage.addEventListener("click", function() {
+localStorage.clear();
+console.log(localStorage);
+})
+
